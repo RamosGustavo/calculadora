e9f0bcca2c1569e37200958a86707d4351c2db4f
RamosGustavo <gr4030077@gmail.com>
2022-07-04 16:57:32 -0300

    correcion de errores

--- Display.js
+++ Display.js
@@ -10,12 +10,12 @@ class Display {
             sumar: '+',
             dividir: '%',
             multiplicar: 'x',
-            restar: '-', 
+            restar: '-',
         }
     }
 
     borrar() {
-        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.valorActual = this.valorActual.toString().slice(0, -1);
         this.imprimirValores();
     }
 
@@ -35,7 +35,7 @@ class Display {
     }
 
     agregarNumero(numero) {
-        if(numero === '.' && this.valorActual.includes('.')) return
+        if (numero === '.' && this.valorActual.includes('.')) return
         this.valorActual = this.valorActual.toString() + numero.toString();
         this.imprimirValores();
     }
@@ -49,7 +49,7 @@ class Display {
         const valorAnterior = parseFloat(this.valorAnterior);
         const valorActual = parseFloat(this.valorActual);
 
-        if( isNaN(valorActual)  || isNaN(valorAnterior) ) return
+        if (isNaN(valorActual) || isNaN(valorAnterior)) return
         this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
     }
 }
